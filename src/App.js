063f26c017152731a488840d7334d2e9bd5c2730
faisalahmed7 <faisalahmeddiu@gmail.comm>
2022-03-26 14:09:32 +0600

    Fake Data Created & Loaded by useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-
 import './App.css';
-
+import Books from './components/Books/Books';
 import Header from './components/Header/Header';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      
+      <Books></Books>
     </div>
   );
 }
 
 export default App;
+
--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,15 +1,15 @@
 import './Books.css'
 
 import React, { useEffect, useState } from 'react';
-import Book from '../Book/Book';
-import Cart from '../Cart/Cart';
+
+
 
 
 
 const Books = () => {
     const [books, setBooks] = useState([])
-    const [cart, setCart] = useState([])
-    const [random, setRandom] = useState([]);
+   
+    
 
     useEffect(() => {
         fetch('data.json')
@@ -17,36 +17,22 @@ const Books = () => {
             .then(data => setBooks(data))
     }, [])
 
-    const handleCart = (book) => {
-        const newCart = [...cart, book];
-        setCart(newCart);
-    }
+    
 
-    // Generate Random Data From Cart
-    const randDataShow = () => {
-        if (cart.length !== 0) {
-            const randomIndex = Math.floor(Math.random() * cart.length);
-            const item = cart[randomIndex];
-            setRandom([item]);
-        }
-    }
+   
 
-    // Clear Data From Cart
-    const clearData = () => {
-        setCart([]);
-        setRandom([]);
-    }
+    
 
     return (
         <div className='shop-container'>
             <div className='books-container'>
                 {
-                    books.map(book => <Book key={book.isbn13} book={book} handleCart={handleCart} ></Book>)
+                    books.map(book => console.log(book))
                 }
             </div>
 
             <div className="cart-container">
-                <Cart clearData={clearData} rand={random} randDataShow={randDataShow} cart={cart}></Cart>
+                
             </div>
 
         </div>
